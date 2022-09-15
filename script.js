f74cb0ec22b75c72f0f5c0ccf9024bd12656696e
FerdousFardin <fardinh39u@gmaile.com>
2022-09-15 16:40:02 +0600

    breakpoints edited

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ fetch("./texts.json")
 // checks the user typed character and displays accordingly
 const typeController = (e) => {
   const newLetter = e.key;
+  if (e.keyCode == 32) e.preventDefault();
 
   // Handle backspace press
   if (newLetter == "Backspace") {
